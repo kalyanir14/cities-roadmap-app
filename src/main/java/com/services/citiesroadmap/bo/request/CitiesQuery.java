9c53b2273080a19e544144adf56eb8deeedd1723
kalyanir14 <69640974+kalyanir14@users.noreply.github.com>
2020-08-13 14:10:27 -0400

    Update CitiesQuery.java

--- src/main/java/com/services/citiesroadmap/bo/request/CitiesQuery.java
+++ src/main/java/com/services/citiesroadmap/bo/request/CitiesQuery.java
@@ -4,15 +4,18 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import javax.ws.rs.QueryParam;
+import javax.validation.constraints.NotNull;
 
 @Setter
 @Getter
 public class CitiesQuery {
     @QueryParam(value = "origin")
     @ApiModelProperty(value = "origin", required = false)
+    @NotNull
     private String origin;
 
     @QueryParam(value = "destination")
     @ApiModelProperty(value = "destination", required = false)
+    @NotNull
     private String destination;
 }
