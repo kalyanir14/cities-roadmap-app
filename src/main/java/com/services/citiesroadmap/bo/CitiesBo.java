63f7e5a3a88741a4b7058e5cd65ecca0ab8bd6c5
kalyanir14 <69640974+kalyanir14@users.noreply.github.com>
2020-08-13 12:16:57 -0400

    Create CitiesBo.java

---
+++ src/main/java/com/services/citiesroadmap/bo/CitiesBo.java
@@ -0,0 +1,7 @@
+package com.services.citiesroadmap.bo;
+
+import com.services.citiesroadmap.bo.model.CitiesRequest;
+
+public interface CitiesBo {
+    String getCitiesConnected(CitiesRequest request);
+}
