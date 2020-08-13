2e8ec3fdbbd513df1691d31ea1a3ae24d3d64d94
kalyanir14 <69640974+kalyanir14@users.noreply.github.com>
2020-08-13 13:45:08 -0400

    Create MockData.java

---
+++ src/test/java/com/services/citiesroadmap/testUtils/MockData.java
@@ -0,0 +1,29 @@
+package com.services.citiesroadmap.testUtils;
+
+import com.services.citiesroadmap.bo.model.CitiesRequest;
+import com.services.citiesroadmap.bo.request.CitiesQuery;
+
+public class MockData {
+
+    public static CitiesQuery buildCitiesQueryRequest() {
+        CitiesQuery request = new CitiesQuery();
+        request.setOrigin("Boston");
+        request.setDestination("Newark");
+        return request;
+    }
+
+    public static CitiesRequest buildCitiesRequest() {
+        CitiesRequest request = new CitiesRequest();
+        request.setOrigin("Newark");
+        request.setDestination("New York");
+        return request;
+    }
+
+    public static CitiesRequest buildNoCitiesRequest() {
+        CitiesRequest request = new CitiesRequest();
+        request.setOrigin("Test1");
+        request.setDestination("York");
+        return request;
+    }
+
+}
