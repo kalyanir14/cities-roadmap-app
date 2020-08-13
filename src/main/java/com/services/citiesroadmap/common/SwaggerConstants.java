bbd1429352d67c9abc2a5966e5a1cdbc37827ae8
kalyanir14 <69640974+kalyanir14@users.noreply.github.com>
2020-08-13 12:39:52 -0400

    Create SwaggerConstants.java

---
+++ src/main/java/com/services/citiesroadmap/common/SwaggerConstants.java
@@ -0,0 +1,12 @@
+package com.services.citiesroadmap.common;
+
+public class SwaggerConstants {
+    public static final String MAIL_ID = "Team@test.com";
+    public static final String DEV = "Development";
+    public static final String TITLE = "Service";
+    public static final String SWAGGER_VERSION = "v1";
+    public static final String DESC = "Application";
+    public static final String CONTROLLER_PATH = "com.services.citiesroadmap.controller";
+    public static final String PROFILE = "swagger";
+    public static final String URL = "testURL";
+}
