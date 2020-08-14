94876f6c13808b94615fe590bfcf95afa97e0ba7
kalyanir14 <69640974+kalyanir14@users.noreply.github.com>
2020-08-14 12:52:52 -0400

    Delete CitiesBo.java

--- src/main/java/com/services/citiesroadmap/bo/CitiesBo.java
+++
@@ -1,7 +0,0 @@
-package com.services.citiesroadmap.bo;
-
-import com.services.citiesroadmap.bo.model.CitiesRequest;
-
-public interface CitiesBo {
-    String getCitiesConnected(CitiesRequest request);
-}
